de7c74adb4f2704ac30ba4736656c3517853ddff
Dani <balintdani1998@gmail.com>
2021-01-05 21:04:49 +0100

    first push

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,18 +1,18 @@
 from sklearn import datasets
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 boston = datasets.load_boston()
 
-lr = LinearRegression()
+lr = LinearRegression(normalize=True) #object can automatically normalize (or scale) inputs
+
 #. Pass the independent and dependent variables to the fit method of LinearRegression:
 lr.fit(boston.data, boston.target)
-LinearRegression(copy_X=True, fit_intercept=True, n_jobs=1,normalize=False)
+
 #Now, get the 10-fold cross-validated predictions
+predictions_cv = cross_val_predict(lr, boston.data, boston.target,cv=10)
 
-lr2 = LinearRegression(normalize=True) #object can automatically normalize (or scale) inputs
-lr2.fit(boston.data, boston.target)
-LinearRegression(copy_X=True, fit_intercept=True, normalize=True)
-predictions2 = lr2.predict(boston.data)
-
-
-
+# get the errors
+print('MAE: ', mean_absolute_error(boston.target, predictions_cv))
+print('MSE: ', mean_squared_error(boston.target, predictions_cv))
