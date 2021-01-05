63a9f2531d2217975af8fbfbddc4d1d30a6f7ded
Dani <balintdani1998@gmail.com>
2021-01-05 21:45:32 +0100

    tuning parameters

---
+++ tuningParameters.py
@@ -0,0 +1,35 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+# Grid Search for Algorithm Tuning
+import numpy
+from pandas import read_csv
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import GridSearchCV
+filename = 'diabetes.data.csv'
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataframe = read_csv(filename, names=names)
+array = dataframe.values
+X = array[:,0:8]
+Y = array[:,8]
+alphas = numpy.array([1,0.1,0.01,0.001,0.0001,0])
+param_grid = dict(alpha=alphas)
+model = Ridge()
+grid = GridSearchCV(estimator=model, param_grid=param_grid)
+grid.fit(X, Y)
+print(grid.best_score_)
+print(grid.best_estimator_.alpha)
+print('------------------------------------------------')
+
+
+# Randomized for Algorithm Tuning
+from sklearn.model_selection import RandomizedSearchCV
+from scipy.stats import uniform
+param_grid = {'alpha': uniform()}
+model = Ridge()
+rsearch = RandomizedSearchCV(estimator=model, param_distributions=param_grid, n_iter=100,
+random_state=7)
+rsearch.fit(X, Y)
+print(rsearch.best_score_)
+print(rsearch.best_estimator_.alpha)
+print('------------------------------------------------')
